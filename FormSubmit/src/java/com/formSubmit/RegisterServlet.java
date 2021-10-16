23ee140b1a0cdc1dadd7cca060641cf495e470fe
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-10-16 10:36:21 -0700

    updated with request dispatcher and include

--- FormSubmit/src/java/com/formSubmit/RegisterServlet.java
+++ FormSubmit/src/java/com/formSubmit/RegisterServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.*;
  * @author Asus
  */
 public class RegisterServlet extends HttpServlet{
+    @Override
     public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
@@ -31,17 +32,33 @@ public class RegisterServlet extends HttpServlet{
             out.println("<h2>Your Responses submitted successfully</h2>");
         
         
-        if(cond.equals("checked")){
-            out.println("<h2> Name :"+ name +"</h2>");
-            out.println("<h2> password :"+ password +"</h2>");
-            out.println("<h2> email :"+ email+ "</h2>");
-            out.println("<h2> Gender :"+ gender+ "</h2>");
-            out.println("<h2> Course :"+ course+ "</h2>");
+            if(cond.equals("checked")){
+                out.println("<h2> Name :"+ name +"</h2>");
+                out.println("<h2> password :"+ password +"</h2>");
+                out.println("<h2> email :"+ email+ "</h2>");
+                out.println("<h2> Gender :"+ gender+ "</h2>");
+                out.println("<h2> Course :"+ course+ "</h2>");
+             
+                //JDBC 
+                // Saved to JDBC
+                RequestDispatcher rd = request.getRequestDispatcher("success");
+                rd.forward(request, response);
+                }
+            else{
+                out.println("<h2>You have not accepted terms and conditions</h2>");
+            }
         }
-        }
+        
+        
+        
+        
         else{
             out.println("<h2>You have not accepted terms and conditions</h2>");
-        
+            // WE should include output of index.html
+            //get the object of RequestDispacher
+            RequestDispatcher rd = request.getRequestDispatcher("index.html");
+            rd.include(request, response); // we include output of another index.html
+            
         }
     }
     
